b7e092814c5309913ebb94c1613309071661cb06
Spica234 <792870+spica234@users.noreply.github.com>
2025-01-24 04:22:01 +0530

    1. Migrate to Angular 18: Core,cli,cdk,material,schematics
    - Migrate to Angular 18: Core,cli,cdk,material,schematics
    
    First run `ng update` to get the upgradable packages and then run the following commands(If available):
    
    ```bash
    ng update @angular/cli@18;
    ng update @angular/core@18;
    ng update @angular/cdk@18;
    ng update @angular/material@18;
    ng update @angular-eslint/schematics@18;
    ```
    
    Signed-off-by: Spica234 <792870+spica234@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { AppComponent } from '@/app/app.component';
 import { routes } from '@/app/app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
